9207e7b7e8bd59a999f4a7de83779efa3026206f
Brielleblocks <99747488+Decreedblocks@users.noreply.github.com>
2023-06-13 11:32:12 +0100

    landing page

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get a reference to the "Go To Home" button
+const ctaButton = document.getElementById("cta-button");
+
+// Add a click event listener to the button
+ctaButton.addEventListener("click", () => {
+  // Redirect the user to the main page
+  window.location.href = "https://serene-dango-8c9ef6.netlify.app/";
+});
